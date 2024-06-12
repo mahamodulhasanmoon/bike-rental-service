a584316c4d5d0683f6e398fef1fd575e25c1b010
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-06-12 19:39:45 +0600

    added initial files

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import app from './app/app';
+import { mongoUrl, port } from './config';
+// import { connectMongoDB } from './db/dbConnect';
+
+(async () => {
+  try {
+    if (mongoUrl) {
+      // await connectMongoDB(mongoUrl);
+      app.listen(port, () => {
+        // eslint-disable-next-line no-console
+        console.log(`server is running at ${port}`);
+      });
+    } else {
+      // eslint-disable-next-line no-console
+      console.error('MongoDB URL is not defined.');
+    }
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Failed to connect to the database:', error);
+  }
+})();
